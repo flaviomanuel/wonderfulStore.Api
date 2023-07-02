77438eef6ede6e240e531bb5a8946e59b458041e
Flávio Manuel <flaviomanuel258@gmail.com>
2023-07-01 23:10:46 -0300

    remove useless using

--- WonderfulStore.Application/Commands/AddProduct/AddProductCommandHandler.cs
+++ WonderfulStore.Application/Commands/AddProduct/AddProductCommandHandler.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MediatR;
 using WonderfulStore.Core.Entities;
 using WonderfulStore.Core.Interfaces;

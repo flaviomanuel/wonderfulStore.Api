c8c5d14929ca56ab3c440f27be21551349b54cc9
Flávio Manuel <flaviomanuel258@gmail.com>
2023-07-03 23:17:15 -0300

    feat(PromotionController): create route to list all promotionsTypes

---
+++ WonderfulStore.Api/Controllers/PromotionController.cs
@@ -0,0 +1,24 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WonderfulStore.Application.Queries.ListAllPromotionTypes;
+
+namespace WonderfulStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class PromotionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PromotionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("ListAllPromotionTypes")]
+        public  async Task<IActionResult> ListAllPromotionTypes( ){
+            var query = new ListAllPromotionTypesQuery();
+
+            var result = await _mediator.Send(query);
+
+            return Ok(result.ToList());
+        }
+    }
+}
